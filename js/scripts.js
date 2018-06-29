157e72845137f7eda5caf9ba2ab9208d63558e07
Jacob Okia <cobs399@gmail.com>
2018-06-29 13:19:53 +0300

    created a function that tests if a year is divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,13 @@
 var leapYear = function(year){
     return false;
 };
-
+var leapYear = function(year){
+    if (year % 4 ===0){
+      return true;
+    } else {
+      return false;
+    }
+}
 
 
 
